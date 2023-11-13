199250cf4a0a6e43e65f13e728844c712135f5ba
rakaisback07 <107905965+rakaisback07@users.noreply.github.com>
2023-11-13 18:22:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validateLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (username === 'Sakshi' && password === 'Dolly') {
+    if (username === 'Sakshi' && password === 'Sakshi123') {
         // Successful login
         window.location.href = 'success.html'; // Redirect to a success page
     } else {
